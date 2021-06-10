0fae53a7f328fa8a22c7d9f1f11a939ee704149d
alisidley <46480832+alisidley@users.noreply.github.com>
2021-06-10 14:22:42 +0200

    Add files via upload
    new script just to plot adc histogram for radioactive source

---
+++ Calibration/share/plot_ADCcount_radio.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  2 13:09:25 2021
+Analysis of detector signals
+@NookKosterUVa, @haslbeck
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+import argparse
+import csv
+
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f',type=str,default = None, help='name patter csv')
+parser.add_argument('-p',type=str,default='../data/',help='path to txt file w muon signal')
+parser.add_argument('-n',type=int,default=1,help='number of files')
+args = parser.parse_args()
+
+path_to_files = args.p
+plt.close('all')
+
+ 
+def get_data(fname):
+    """ Get data (voltage and time measurements) from csv-file: start at peak, just after t=0 """
+
+    print('get', fname)
+
+
+    adc = [] 
+    if "NewFile" in fname: adc2 = []
+    with open(fname ) as csv_file: 
+        csv_reader = csv.reader(csv_file, delimiter= ',')
+        for i,row in enumerate(csv_reader): 
+            if "NewFile" in fname:
+                if i<6: continue  # skip header
+            
+                adc.append(float(row[2]))
+            else:
+                adc.append(float(row[4]))
+            pass
+
+        index_max = adc.index(max(adc))     #cut all data before peak (~t=0)
+        peak = adc[index_max:]
+    fig = plt.figure(figsize=(12,7))
+    plt.xlabel("ADC")
+    plt.title("ADC count for radioactive source")
+    plt.hist(adc)
+    plt.savefig("adc_radioactive_histogram.png")
+    return peak, adc_hist
