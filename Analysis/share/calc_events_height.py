27de63867344181b4f895e3ffe192a72a65f4ab6
haslbeck <78813607+haslbeck@users.noreply.github.com>
2021-06-17 14:07:16 +0200

    analytical estimate of events vs distance of detectors -fh

---
+++ Analysis/share/calc_events_height.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def invTan(h):
+    return np.arctan(10/h)
+
+def events(h):
+    it = invTan(h)
+    return 0.5 * (it + np.sin(it) * np.cos(it) )
+   
+   
+label = "events $\sim \chi + sin(\chi)\cdot cos(\chi)$\n with $\chi = tan^{-1}(\\frac{10cm}{h})$" 
+
+h = np.linspace(5,150,145)
+plt.plot(h, events(h),label=label)
+plt.xlabel("distance between scintillator layers [cm]")
+plt.ylabel("events [a.u.]")
+plt.legend()
+
+plt.draw()
+plt.savefig("../data/events_vs_height.png")
+
+plt.pause(1)
+input('press any key to close')
+plt.close('all')
+
+
+print('goodbye')
