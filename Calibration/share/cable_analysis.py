dde6c9ad226b1731f090989b0270be0f4836f5ae
haslbeck <78813607+haslbeck@users.noreply.github.com>
2021-06-22 01:01:09 +0200

    cleaned up plots for final report, fixed auto labels, added manual labels -fh

--- Calibration/share/cable_analysis.py
+++ Calibration/share/cable_analysis.py
@@ -14,8 +14,9 @@ parser = argparse.ArgumentParser(description='calibration fit')
 # input file
 parser.add_argument('-f',type=str, nargs='+', 
                     help='csv filename with timestamp, ADC counts')
-parser.add_argument('-path',type=str,default='../data/cable/',required=False,
-                    help='default ../data/cable/')
+parser.add_argument('-path',type=str,default='../data/cable_test/',required=False,
+                    help='default ../data/cable_test/')
+
 # output file
 parser.add_argument('-out_name',type=str,default=None,required=False,
                     help='calibration constants')
@@ -23,14 +24,28 @@ parser.add_argument('-out_path',type=str,default='../data/calibration/',required
                     help='default ../data/calibration/')
 parser.add_argument('-header',type=int,default=3,required=False,
                     help='default 3')
+
+parser.add_argument('-fit',action='store_true',default=False,required=False,
+                    help='perform an exponential fit to time spectrum, default FALSE)')
+parser.add_argument('-norm',action='store_false',default=True,required=False,
+                    help='normalise histograms, default True')
+
 parser.add_argument('-dist',type=str,default="cable",required=False,
                     help='distinction keyword for classes eg. "cable" (default)')
 parser.add_argument('-dist2',type=str,default=None,required=False,
                     help='distinction 2 keyword for classes eg. None (default)')
-
+parser.add_argument('-label',type=str,default=None,required=False,
+                    help='histogram label')
+parser.add_argument('-labeld',type=str,default=None,required=False,
+                    help='histogram label2 matching dist')
+parser.add_argument('-labeld2',type=str,default=None,required=False,
+                    help='histogram label2 matching dist2')
 
 args = parser.parse_args()
 
+hist_colors = ["blue", "red", "green"] 
+#["darkblue", "darkred", "darkgreen"]
+fit_colors = ["blue", "red", "green"] 
 
 
 def fit_func(x, a, b):
@@ -38,8 +53,17 @@ def fit_func(x, a, b):
     y = a*np.exp(b*x)
     return y
 
-def exp_pdf_fit(xdata,ydata,p0=[0.1,0.05]):
+def exp_pdf_fit(xdata,ydata,xmin=None,xmax=None,p0=[0.1,0.05]):
     
+    if xmin is not None: 
+        ind = np.where(xdata > xmin)
+        xdata = xdata[ind]
+        ydata = ydata[ind]
+    if xmax is not None:
+        ind = np.where(xdata < xmax)
+        xdata = xdata[ind]
+        ydata = ydata[ind]
+        
     popt, pcov = curve_fit(fit_func, xdata, ydata, p0 = p0)
     
     # print out the fit params
@@ -52,24 +76,30 @@ def exp_pdf_fit(xdata,ydata,p0=[0.1,0.05]):
     return vals , errs 
     
 
-def make_hist(id,data,xname,bins=256,x_range=(0,1023),label=None,alpha=0.35):
+def make_hist(id,data,xname,bins=256,x_range=(0,1023),label=None,alpha=1, FIT = args.fit, NORM = args.norm, color_id = None):
     plt.figure(id)
-    data, bin_x , _ = plt.hist(data,bins=bins,range=x_range,density=True,alpha=alpha,label=label)
+    
+    if color_id is not None: data, bin_x , _ = plt.hist(data,bins=bins,range=x_range,density=NORM,\
+                                                        alpha=alpha,label=label,color = hist_colors[color_id],\
+                                                        ec = hist_colors[color_id], histtype='step')
+    else: data, bin_x , _ = plt.hist(data,bins=bins,range=x_range,density=NORM,alpha=alpha,label=label)
     plt.xlabel(xname)
     plt.ylabel('Entries')
     if label is not None: plt.legend()
 
     
     # fit time diff distr
-    if "Time" in xname:
+    if "Time" in xname and FIT:
         bin_centers = np.array([(bin_x[i]+bin_x[i+1]) for i in range(len(bin_x)-1)]) * 0.5
-        vals, errs = exp_pdf_fit(bin_centers, data)
-        xplot = np.linspace(x_range[0],x_range[1],bins*2)
+        
+        xmin, xmax = 0.5, 5
+        vals, errs = exp_pdf_fit(bin_centers, data, xmin, xmax)
+        xplot = np.linspace(xmin,xmax,bins*2)
         label="%s Fit $a * e^{-\lambda x}$\n"%label
-        label+="$a$ = (%.2e$\pm$%.2e)}\n"%(vals[0],errs[0])
-        label+="$\lambda$ = (%.2e$\pm$%.2e)"%(vals[1],errs[1])
-        plt.plot(xplot, fit_func(xplot, *vals), label = label) 
-    
+        label+="$a$ = (%.1f$\pm$%.1f)\n"%(vals[0],errs[0])
+        label+="$\lambda$ = (%.2f$\pm$%.2f)"%(vals[1],errs[1])
+        if color_id is not None: plt.plot(xplot, fit_func(xplot, *vals), label = label,color = fit_colors[color_id]) 
+        else: plt.plot(xplot, fit_func(xplot, *vals), label = label) 
     plt.legend()
     
 
@@ -82,7 +112,7 @@ files = args.f
 # for combined plot
 adc_scint, adc_cable = [] , []
 timediff_cable, timediff_scint = [] , []
-if DISTINCTION_2 is not None:    adc_else, timediff_else = [] , [] # if 3rd alternative needed
+if DISTINCTION_2 is not None: adc_else, timediff_else = [] , [] # if 3rd alternative needed
 
 for f in files: 
     # remove additional .csv
@@ -121,18 +151,28 @@ for f in files:
     make_hist(1,adc,'ADC',label=f,bins=128)
     make_hist(2,timediff,'Time diff [s]',label=f,x_range=(0,60),bins=60)
 
+#plots combining multiple files
+
+# make nice labels
+label_scint = 'total %s'%DISTINCTION if args.label is None else args.label
+
 dist2_label = DISTINCTION_2 if DISTINCTION_2 is not None else "NOT %s"%DISTINCTION
+label_cable = 'total %s'%dist2_label if args.labeld is None else args.labeld
 
-make_hist(3,adc_cable,'ADC',label='total %s'%DISTINCTION,bins=128)
-make_hist(3,adc_scint,'ADC',label='total %s'%dist2_label,bins=128)
+adc_range, adc_bins = (0,1023), 128
+make_hist(3,adc_cable,'ADC [a.u.]',label=label_cable,x_range=adc_range,bins=adc_bins,color_id = 0)
+make_hist(3,adc_scint,'ADC [a.u.]',label=label_scint,x_range=adc_range,bins=adc_bins,color_id = 1)
 
-make_hist(4,timediff_cable,'Time diff [s]',label='total %s'%DISTINCTION,x_range=(0,60),bins=60)
-make_hist(4,timediff_scint,'Time diff [s]',label='total %s'%dist2_label,x_range=(0,60),bins=60)
-
+# timediff
+time_range , time_bins = (0,10), 20
+make_hist(4,timediff_cable,'Time diff [s]',label=label_cable,x_range=time_range,bins=time_bins,color_id = 0)
+make_hist(4,timediff_scint,'Time diff [s]',label=label_scint,x_range=time_range,bins=time_bins,color_id = 1)
+print(min(timediff_cable),min(timediff_scint))
 
 if DISTINCTION_2 is not None:
-    make_hist(3,adc_else,'ADC',label='total rest',bins=128)
-    make_hist(4,timediff_scint,'Time diff [s]',label='total rest',x_range=(0,60),bins=60)
+    label_else = 'total rest' if args.labeld2 is None else args.labeld2
+    make_hist(3,adc_else,'ADC [a.u.]',label=label_else,x_range=adc_range,bins=adc_bins,color_id = 2)
+    make_hist(4,timediff_else,'Time diff [s]',label=label_else,x_range=time_range,bins=time_bins,color_id = 2)
 
 # close the plot when pressing a key
 plt.draw()
