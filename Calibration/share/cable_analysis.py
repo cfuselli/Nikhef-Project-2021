b8e4ced6f2cfe947b96465a0483c7328a8717ca4
Nikhef Project <nikhefproj@dhcp-135-059.nikhef.nl>
2021-06-21 12:55:54 +0200

    labels were flipped, need to redo cable and scintillator analysis -fh

--- Calibration/share/cable_analysis.py
+++ Calibration/share/cable_analysis.py
@@ -28,6 +28,12 @@ parser.add_argument('-dist',type=str,default="cable",required=False,
 parser.add_argument('-dist2',type=str,default=None,required=False,
                     help='distinction 2 keyword for classes eg. None (default)')
 
+parser.add_argument('-fit',action='store_true',default=False,required=False,
+                    help='perform an exponential fit to time spectrum, default FALSE)')
+parser.add_argument('-norm',action='store_false',default=True,required=False,
+                    help='normalise histograms, default True')
+
+
 
 args = parser.parse_args()
 
@@ -52,16 +58,16 @@ def exp_pdf_fit(xdata,ydata,p0=[0.1,0.05]):
     return vals , errs 
     
 
-def make_hist(id,data,xname,bins=256,x_range=(0,1023),label=None,alpha=0.35):
+def make_hist(id,data,xname,bins=256,x_range=(0,1023),label=None,alpha=0.35, FIT = False, NORM = True):
     plt.figure(id)
-    data, bin_x , _ = plt.hist(data,bins=bins,range=x_range,density=True,alpha=alpha,label=label)
+    data, bin_x , _ = plt.hist(data,bins=bins,range=x_range,density=NORM,alpha=alpha,label=label)
     plt.xlabel(xname)
     plt.ylabel('Entries')
     if label is not None: plt.legend()
 
     
     # fit time diff distr
-    if "Time" in xname:
+    if "Time" in xname and FIT:
         bin_centers = np.array([(bin_x[i]+bin_x[i+1]) for i in range(len(bin_x)-1)]) * 0.5
         vals, errs = exp_pdf_fit(bin_centers, data)
         xplot = np.linspace(x_range[0],x_range[1],bins*2)
@@ -123,16 +129,16 @@ for f in files:
 
 dist2_label = DISTINCTION_2 if DISTINCTION_2 is not None else "NOT %s"%DISTINCTION
 
-make_hist(3,adc_cable,'ADC',label='total %s'%DISTINCTION,bins=128)
-make_hist(3,adc_scint,'ADC',label='total %s'%dist2_label,bins=128)
+make_hist(3,adc_cable,'ADC',label='total %s'%dist2_label,bins=128, FIT = args.fit, NORM = args.norm)
+make_hist(3,adc_scint,'ADC',label='total %s'%DISTINCTION,bins=128, FIT = args.fit, NORM = args.norm)
 
-make_hist(4,timediff_cable,'Time diff [s]',label='total %s'%DISTINCTION,x_range=(0,60),bins=60)
-make_hist(4,timediff_scint,'Time diff [s]',label='total %s'%dist2_label,x_range=(0,60),bins=60)
+make_hist(4,timediff_cable,'Time diff [s]',label='total %s'%dist2_label,x_range=(0,60),bins=60, FIT = args.fit, NORM = args.norm)
+make_hist(4,timediff_scint,'Time diff [s]',label='total %s'%DISTINCTION,x_range=(0,60),bins=60, FIT = args.fit, NORM = args.norm)
 
 
 if DISTINCTION_2 is not None:
-    make_hist(3,adc_else,'ADC',label='total rest',bins=128)
-    make_hist(4,timediff_scint,'Time diff [s]',label='total rest',x_range=(0,60),bins=60)
+    make_hist(3,adc_else,'ADC',label='total rest',bins=128, FIT = args.fit, NORM = args.norm)
+    make_hist(4,timediff_else,'Time diff [s]',label='total rest',x_range=(0,60),bins=60, FIT = args.fit, NORM = args.norm)
 
 # close the plot when pressing a key
 plt.draw()
