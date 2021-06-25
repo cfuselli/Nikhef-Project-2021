f8b83fa25215bcf51f79f3efd3d73db0e90c4785
haslbeck <nikhefproj@dhcp-135-059.nikhef.nl>
2021-06-25 10:45:33 +0200

    class to convert adc to mv using calib data -fh

---
+++ Readout_copy/share/converter.py
@@ -0,0 +1,21 @@
+# class to read in calibration constants and convert ADC to mV
+
+class Converter():
+    def __init__(self, rfile, path):
+
+        # load csv file and save
+
+    def __del__(self):
+
+        # close file
+
+    def adc2mv(self, detector, adc):
+
+        return mv
+
+    
+
+    
+
+
+
