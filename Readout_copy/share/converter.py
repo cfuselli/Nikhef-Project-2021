e7396c52acc71a10d6237550299ae115d2d2f69a
haslbeck <nikhefproj@dhcp-135-059.nikhef.nl>
2021-06-25 22:57:56 +0200

    push-fh

--- Readout_copy/share/converter.py
+++ Readout_copy/share/converter.py
@@ -7,12 +7,16 @@ import io
 import numpy as np
 import csv
 
+import matplotlib.pyplot as plt
+
 class Converter():
     """ convert ADC to mV using calibration data extracted by polynomial fit """
     
-    def __init__(self, rfile = 'test_calib_const.csv', path = '../../Calibration/data/final_calib/',\
+    def __init__(self, rfile = 'All_Fit_Params.csv', path = '../../Calibration/data/final_calib/',\
                 detectors = ['Carlo','Florian','BenRevival','Franko','Niels',\
-                            'M', 'Noor', 'rens', 'MattiaCosmicWatch', 'Cecile', 'ZenBenMaster'],
+                             'M', 'Noor', 'rens', 'MattiaCosmicWatch', 'Cecile','Oline',\
+                             'dummy'
+                             ],
                 rank = 11, header = 2, verbose = False
                 ):
         """ save calibration constants per detector """
@@ -21,13 +25,12 @@ class Converter():
         self.constants = {}
         for detector in detectors: self.constants[detector] = [] # ignore par errors
         
-        
         # read in file
         detector = None
         if rfile[-4:] != '.csv': rfile+= '.csv'
         if path[-1] != '/': path+= '/'
     
-        with open(path + rfile) as csvfile:
+        with open(path + rfile, encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
             for i, line in enumerate(reader):
             
@@ -43,19 +46,42 @@ class Converter():
         
         # feedback
         if verbose:
-            for (detector, vals) in self.constants.items(): print(detector, [type(val) for val in vals])
+            for (detector, vals) in self.constants.items(): print(detector, vals)
 
     def adc2mv(self, detector, adc):
         """ convert adc to mV using by evaluating the polynomial fit function """
         coeff = self.constants[detector]
+        coeff = np.flip(coeff)
         mv = np.polyval(coeff, adc)
+        #mv  = 0.
+        #for order,par in enumerate(coeff): mv+=par*np.power(adc,order)
         return mv
 
 
     
 if __name__ == '__main__':
     c = Converter()
-    print(c.adc2mv('Carlo',2))
+    print(c.adc2mv('Noor',159.))
+
+    test = np.linspace(0,100,1000)
+    print(c.constants["Noor"])
+    #plt.figure(1)
+    #plt.plot(test,c.adc2mv("Noor",test))
+    #plt.yscale('log')
+    #plt.show()
+    #input()
+
+    plt.figure(2)
+    x = np.linspace(0,1000,1000)
+
+
+    pars = [0.,0.,0.,0.,0.,-2.0000000e-06, 5.0299999e-04, -5.8260001e-02, 2.9504969e+00,1.7245186e+01]
+    pars.reverse()
+    print(pars)
+    plt.plot(x,np.polyval(pars  , x))
+    #plt.yscale('log')
+    plt.show()
+    input()
     
 
 
